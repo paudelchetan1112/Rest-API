91220730c3bcbbd0a30dc9348a187936f45f0ea2
chetan Paudel <paudelchetan1112@gmail.com>
2025-12-02 21:55:04 +0545

    hashing password

--- src/User/userController.ts
+++ src/User/userController.ts
@@ -2,6 +2,7 @@ import type{Request, Response } from "express";
 import type { NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel.ts";
+import bcrypt from 'bcrypt'
 
 const createUser=async(req:Request,res:Response,next:NextFunction)=>{
     console.log(req.body)
@@ -15,7 +16,7 @@ if(!name || !email || !password){
 
 }
 
-//Database call 
+//Database call avoid to insert that email which is already exit
 const user=await userModel.findOne({email});
 
 res.json({message:"user Created"})
@@ -25,5 +26,8 @@ if(user){
     const error=createHttpError(400,"User already exit with this email.")
     return next(error)
 }
+//password->hash
+const hashedPassword=await bcrypt.hash(password,10)
 }
+
 export {createUser}
