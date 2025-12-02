342c76cb980263826159e3dbdc4999a6f084b468
chetan Paudel <paudelchetan1112@gmail.com>
2025-12-02 14:43:35 +0545

    set up express server

--- Server.ts
+++ Server.ts
@@ -1,1 +1,8 @@
-console.log("welcome to the ebook API")
+
+// import express from "express"
+import app from "./src/app.ts";
+// const app=express();
+const PORT=3000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("server is running")
+import express from 'express'
+
+const app = express()
+
+//Routes
+//http methods: Get, Post, put, patch, delete
+app.get('/',(req,res)=>{
+    res.send("welcome to the server")
+})
+export default app
